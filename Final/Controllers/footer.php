ab28a3f8c3eb31c9ce1eb1a8e80779a249ee808b
joegiesing <jrgiesing@email.neit.edu>
2024-05-31 00:38:25 -0400

    final2

--- Final/Controllers/footer.php
+++ Final/Controllers/footer.php
@@ -3,7 +3,7 @@
     </div>
     <!-- <hr>
     <p></p> -->
-    <div class="footer" style="position: absolute; bottom: 0; width:100%; align-content: center; 
+    <div class="footer" style="width:100%; align-content: center; 
     background-color: lightgray; height: 40px; border-top: 1px solid black;">
         <div style="margin-left: 50px">
             &copy;2024 by Joseph Giesing
